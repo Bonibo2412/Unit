caff6f685363477b2e7963df2026750074937120
Practice test <bonniebraer@gmail.com>
2024-02-13 17:47:32 +0300

    Second

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,9 +1,8 @@
-package ru.netology.service;
-
-import ru.netology.service.CashbackHackService;
+package ru.netology;
 
 public class Main {
     public static void main(String[] args) {
+
         CashbackHackService service = new CashbackHackService();
     }
 }
---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+import ru.netology.CashbackHackService;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+@Test
+    public void cashbackUnderLimit() {
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void cashbackOverLimit() {
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void cashbackEqualLimit() {
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+}
