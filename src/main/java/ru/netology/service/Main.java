e84dfb0912684dd3663667fceeefbc0ad001838f
Practice test <bonniebraer@gmail.com>
2024-02-13 17:14:19 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/service/Main.java
@@ -0,0 +1,9 @@
+package ru.netology.service;
+
+import ru.netology.service.CashbackHackService;
+
+public class Main {
+    public static void main(String[] args) {
+        CashbackHackService service = new CashbackHackService();
+    }
+}
