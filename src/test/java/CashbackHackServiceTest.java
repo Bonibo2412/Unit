c3517a69aad6fb3aa5890fe753c123a02383bbff
Practice test <bonniebraer@gmail.com>
2024-02-19 10:31:02 +0300

    Final

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -20,7 +20,7 @@ public class CashbackHackServiceTest {
     @Test
     public void cashbackEqualLimit() {
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
 
     }
